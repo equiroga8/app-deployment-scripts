ceeeac9bb8c14cc0dc7f24ce1e85c5ff8d4fe4a2
equiroga8 <eduqb8@gmail.com>
2019-01-15 18:55:06 +0100

    initial setup finished

--- initialSetup.py
+++ initialSetup.py
@@ -3,12 +3,12 @@ import os
 
 call("pip install termcolor", shell = True)
 call("pip install pexpect", shell = True)
-call("sudo apt-add-repository ppa:ansible/ansible", shell = True)
+call("sudo apt-add-repository -y ppa:ansible/ansible", shell = True)
 call("sudo apt-get update", shell = True)
 call("sudo apt-get -y install ansible", shell = True)
-call ("sudo cp /home/upm/practicas/roles  /etc/ansible/roles")
-call ("sudo cp /home/upm/practicas/hosts  /etc/ansible/hosts")
-call ("sudo cp /home/upm/practicas/playbook.yml  /etc/ansible/playbook.yml")
+call ("sudo mv -f /home/upm/practica/ansible/roles  /etc/ansible/", shell = True)
+call ("sudo mv -f /home/upm/practica/ansible/hosts  /etc/ansible/hosts", shell = True)
+call ("sudo mv -f /home/upm/practica/ansible/playbook.yml  /etc/ansible/playbook.yml", shell = True)
 
 call("wget http://idefix.dit.upm.es/cdps/pfinal/pfinal.tgz", shell = True)
 call("sudo vnx --unpack /home/upm/practica/pfinal.tgz", shell = True)
@@ -16,5 +16,4 @@ os.chdir("/home/upm/practica/pfinal")
 call("bin/prepare-pfinal-vm", shell = True)
 call("sudo vnx -f pfinal.xml --create", shell = True)
 
-
-call('python /home/upm/practica/setup_scripts/rsaKeysSetup.py', shell = True)
+call('python /home/upm/practica/setup_scripts/rsaKeysSetup.py xxxx', shell = True)
