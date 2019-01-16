2fe90759bc6aac2af723feb23c27ffae9126d5ee
rodrimart97 <rodrigo.mmartin@alumnos.upm.es>
2019-01-16 22:24:22 +0100

    Fallo startForever corregido

--- setup_scripts/startForever.py
+++ setup_scripts/startForever.py
@@ -8,4 +8,4 @@ servers = ['s1', 's2', 's3']
 for server in servers:
 
 	print colored("-> Starting service on " + server + ". Type 'forever start ./bin/www'" , 'green')
-	call("ssh -X -t root@"+ server +"\"cd /root/quiz_2019 ; bash\"", shell = True)
+	call("ssh -X -t root@"+ server +" \"cd /root/quiz_2019 ; bash\"", shell = True)
